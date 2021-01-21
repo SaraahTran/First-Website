01c9e45d3da65ecd42b165af4835b85e49fd27df
SaraahTran <77770132+SaraahTran@users.noreply.github.com>
2021-01-21 21:37:35 +1100

    Update app.js

--- app.js
+++ app.js
@@ -67,3 +67,13 @@ window.onload = function() {
 };
 
 
+document.addEventListener('touchstart', this.touchstart);
+document.addEventListener('touchmove', this.touchmove);
+
+function touchstart(e) {
+    e.preventDefault()
+}
+
+function touchmove(e) {
+    e.preventDefault()
+}
