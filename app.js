cfd57aa5ed80d4cfdd336e42419b636a4bc48f84
SaraahTran <77770132+SaraahTran@users.noreply.github.com>
2021-01-21 21:16:53 +1100

    Update app.js

--- app.js
+++ app.js
@@ -67,6 +67,11 @@ window.onload = function() {
 };
 
 
-
+ document.addEventListener('touchmove', function(event) {
+        event = event.originalEvent || event;
+        if (event.scale !== 1) {
+           event.preventDefault();
+        }
+    }, false);
 
 
